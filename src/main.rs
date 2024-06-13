d019c7efc18241bf56663d8b340ff69b92b3e2f5
Joël J. Présent <36854931+joeljpresent@users.noreply.github.com>
2024-06-13 15:18:54 +0200

    implement better Debug for Value

--- src/main.rs
+++ src/main.rs
@@ -31,5 +31,6 @@ fn main() {
     test_str("de");
     test_str("d3:ouii47e2:no0:e");
     test_str("d3:ouili123ei456ee2:nod0:i12eee");
+    test_str(r#"d8:announce41:http://bttracker.debian.org:6969/announce7:comment35:"Debian CD from cdimage.debian.org"10:created by13:mktorrent 1.113:creation datei1671279452e4:infod6:lengthi3909091328e4:name29:debian-11.6.0-amd64-DVD-1.iso12:piece lengthi262144e6:pieces41:(binary blob of the hashes of each piece)ee"#);
     test_bytes(std::fs::read("sunflower.torrent").unwrap().as_ref());
 }
--- src/value.rs
+++ src/value.rs
@@ -1,9 +1,27 @@
+use core::fmt;
 use std::collections::BTreeMap;
 
-#[derive(Debug, Clone)]
+#[derive(Clone)]
 pub enum Value {
     String(Vec<u8>),
     Int(i64),
     List(Vec<Value>),
     Dictionary(BTreeMap<Vec<u8>, Value>),
+}
+
+impl fmt::Debug for Value {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::String(bytes) => write!(f, "{:?}", String::from_utf8_lossy(bytes)),
+            Self::Int(int) => write!(f, "{}", int),
+            Self::List(list) => write!(f, "{:?}", list),
+            Self::Dictionary(dict) => write!(
+                f,
+                "{:?}",
+                dict.iter()
+                    .map(|(key, val)| (String::from_utf8_lossy(key), val))
+                    .collect::<BTreeMap<_, _>>()
+            ),
+        }
+    }
 }
